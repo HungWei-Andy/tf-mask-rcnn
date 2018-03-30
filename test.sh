07c841e4d65938b4d1607b811172e726ef4c7200
andy.hw <r05942039@ntu.edu.tw>
2018-03-30 23:38:59 +0800

    checkpoint

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
-source .env/bin/activate
 LOG="logs/test_resnet50_fpn_batch1.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 time python ./lib/test.py
--- train.sh
+++ train.sh
@@ -1,7 +1,3 @@
-while true
-do
-source .env/bin/activate
 LOG="logs/resnet50_fpn_batch1.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
-time python3 ./lib/train.py
-done
+time python ./lib/train.py
